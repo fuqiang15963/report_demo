a3eaaaa4449ddc983d0baa69614e85029762543d
fq <493417683@qq.com>
2018-10-18 17:19:15 +0800

    github 测试

--- src/main/java/com/yinhai/report/web/HelloController.java
+++ src/main/java/com/yinhai/report/web/HelloController.java
@@ -1,12 +1,15 @@
 package com.yinhai.report.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${user.name[0]}")
+    private String name;
     @RequestMapping("/hello")
     public String index(){
-        return "Hello World";
+        return name;
     }
 }
